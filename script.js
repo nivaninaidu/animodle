cb7edefb2ec22a0a0865f7a0e8a65ccaea1122f2
Nivani <64935767+nivaninaidu@users.noreply.github.com>
2022-05-06 09:38:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { WORDS } from "words.js";
+import { WORDS } from "./words.js";
 
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
@@ -6,6 +6,7 @@ let currentGuess = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString)
+
 function initBoard() {
     let board = document.getElementById("game-board");
 
