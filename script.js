62b639cf680b206206bbb310b10f8d5519b4f95c
Nivani <64935767+nivaninaidu@users.noreply.github.com>
2022-07-22 08:42:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ function initBoard() {
 }
 
 initBoard()
+document.addEventListener("keyup", (e) => {
+
+    if (guessesRemaining === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
